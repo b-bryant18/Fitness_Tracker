06a881b25b34adcecbfe4225f24d722e13b07fa6
Brad Bryant <bradleybryant86@gmail.com>
2020-02-11 22:17:19 -0600

    Copied files into repo

---
+++ models/workout.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter an exercise type" //cardio or strength
+            },
+            
+
+            name: {  
+                type: String,
+                trim: true,
+                required: "Enter an exercise" //More specific to the exercise
+            },
+
+            weight: {
+                type: Number
+            },
+
+            sets: {
+                type: Number
+            },
+
+            reps: {
+                type: Number
+            },
+
+            duration: {
+                type: Number
+            },
+
+            distance: {
+                type: Number
+            }
+
+        }
+    ]
+})
+
+const workout = mongoose.model("workout", workoutSchema);
+module.exports = workout;
---
+++ routes/api-routes.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const workout = require("../models/workout");
+
+//router.post, router.put, router.delete just like app.get
